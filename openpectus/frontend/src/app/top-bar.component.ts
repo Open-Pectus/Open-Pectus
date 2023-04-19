6e8814d155779b84e7a276a85c256664c45b3902
Jan H. Knudsen <jhk@mjolner.dk>
2023-04-19 12:10:35 +0200

    Using codicon icon font to get a generic user profile icon.

--- openpectus/frontend/src/app/top-bar.component.ts
+++ openpectus/frontend/src/app/top-bar.component.ts
@@ -6,7 +6,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: `
     <div class="w-full h-14 flex items-center bg-sky-600 text-white relative">
       <div class="absolute-center text-3xl font-bold">Open Pectus</div>
-      <div class="absolute right-4 codicon codicon-account">user</div>
+      <div class="absolute right-4 codicon codicon-account !text-3xl"></div>
 
     </div>
   `,
