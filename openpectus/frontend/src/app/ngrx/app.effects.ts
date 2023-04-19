623e004858dac128a8fcc0dcf3b099aab449b609
Jan H. Knudsen <jhk@mjolner.dk>
2023-04-19 15:48:10 +0200

    Removed testing component

--- openpectus/frontend/src/app/ngrx/app.effects.ts
+++ openpectus/frontend/src/app/ngrx/app.effects.ts
@@ -1,16 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { AppActions } from './app.actions';
-import { map } from 'rxjs';
+import { Actions } from '@ngrx/effects';
 
 @Injectable()
 export class AppEffects {
-  testEffect = createEffect(() => this.actions.pipe(
-    ofType(AppActions.aTestAction),
-    map(({aString}) => {
-      window.alert(aString);
-    })
-  ), {dispatch: false});
-
   constructor(private actions: Actions) {}
 }
--- openpectus/frontend/src/app/ngrx/app.reducer.ts
+++ openpectus/frontend/src/app/ngrx/app.reducer.ts
@@ -1,20 +1,12 @@
-import { createReducer, on } from '@ngrx/store';
-import { AppActions } from './app.actions';
-import produce from 'immer';
+import { createReducer } from '@ngrx/store';
 
 export const appFeatureKey = 'app';
 
 export interface AppState {
-  someValue: string;
 }
 
-const initialState: AppState = {
-  someValue: 'test string',
-};
+const initialState: AppState = {};
 
 export const appReducer = createReducer(initialState,
-  on(AppActions.aTestAction, (state, {aString}) => produce(state, draft => {
-    draft.someValue = aString;
-  }))
 );
 
