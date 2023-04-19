74a96ff195f6277fefd1e34af537c4119ed4f2e5
Jan H. Knudsen <jhk@mjolner.dk>
2023-04-19 15:48:49 +0200

    Pulled routing strings into exported consts

--- openpectus/frontend/src/app/app-routing.module.ts
+++ openpectus/frontend/src/app/app-routing.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+export const dashboardUrlPart = 'dashboard';
+export const detailsUrlPart = 'details';
+
 const routes: Routes = [
   {
-    path: 'dashboard',
+    path: `${dashboardUrlPart}`,
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
   },
   {
-    path: 'details',
+    path: `${detailsUrlPart}`,
     loadChildren: () => import('./details/details.module').then(m => m.DetailsModule),
   },
   {
     path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
-  }
+    redirectTo: `${dashboardUrlPart}`,
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- openpectus/frontend/src/app/details/details-routing.module.ts
+++ openpectus/frontend/src/app/details/details-routing.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { UnitDetailsComponent } from './unit-details.component';
 import { BatchJobDetailsComponent } from './batch-job-details.component';
 
+export const processUnitUrlPart = 'process_unit';
+export const batchJobUrlPart = 'batch_job';
 export const unitIdParamName = 'unit_id';
 export const batchJobIdParamName = 'batch_job_id';
 
 const routes: Routes = [
   {
-    path: `unit/:${unitIdParamName}`,
+    path: `${processUnitUrlPart}/:${unitIdParamName}`,
     component: UnitDetailsComponent,
   },
   {
-    path: `batch_job/:${batchJobIdParamName}`,
+    path: `${batchJobUrlPart}/:${batchJobIdParamName}`,
     component: BatchJobDetailsComponent,
   },
 ];
