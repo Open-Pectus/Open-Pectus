490caa549a77b3ac34bf5cbd57d4d83a0f728c78
Jan H. Knudsen <jhk@mjolner.dk>
2023-04-19 15:48:49 +0200

    Pulled routing strings into exported consts

--- openpectus/frontend/src/app/dashboard/process-units/dashboard-process-units.component.ts
+++ openpectus/frontend/src/app/dashboard/process-units/dashboard-process-units.component.ts
@@ -2,14 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { DashboardSelectors } from '../../ngrx/dashboard.selectors';
 import { Router } from '@angular/router';
+import { processUnitUrlPart } from '../../details/details-routing.module';
+import { detailsUrlPart } from '../../app-routing.module';
 
 @Component({
   selector: 'app-dashboard-process-units',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div class="flex">
-      <app-process-unit-card *ngFor="let processUnit of (processUnits | ngrxPush)" [processUnit]="processUnit"
-                             class="w-80"
+      <app-process-unit-card *ngFor="let processUnit of (processUnits | ngrxPush)" [processUnit]="processUnit" class="w-80"
                              (click)="onCardClick(processUnit.id)"></app-process-unit-card>
     </div>
   `,
@@ -20,6 +21,6 @@ export class DashboardProcessUnitsComponent {
   constructor(private store: Store, private router: Router) {}
 
   onCardClick(id: number) {
-    this.router.navigate(['details', 'unit', id]).then();
+    this.router.navigate([detailsUrlPart, processUnitUrlPart, id]).then();
   }
 }
