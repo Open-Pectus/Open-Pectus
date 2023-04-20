67e8e29e36f3f3682b73456ac557cdcf07638ca4
Jan H. Knudsen <jhk@mjolner.dk>
2023-04-20 08:55:31 +0200

    Made process unit cards more repsonsive design.

--- openpectus/frontend/src/app/dashboard/process-units/dashboard-process-units.component.ts
+++ openpectus/frontend/src/app/dashboard/process-units/dashboard-process-units.component.ts
@@ -9,8 +9,8 @@ import { detailsUrlPart } from '../../app-routing.module';
   selector: 'app-dashboard-process-units',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="grid grid-cols-3 gap-6">
-      <app-process-unit-card *ngFor="let processUnit of (processUnits | ngrxPush)" [processUnit]="processUnit" class="w-80"
+    <div class="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <app-process-unit-card *ngFor="let processUnit of (processUnits | ngrxPush)" [processUnit]="processUnit"
                              (click)="onCardClick(processUnit.id)"></app-process-unit-card>
     </div>
   `,
--- openpectus/frontend/src/app/dashboard/process-units/process-unit-card.component.ts
+++ openpectus/frontend/src/app/dashboard/process-units/process-unit-card.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ProcessUnit, ProcessUnitStateEnum } from '../../api';
+import { UtilMethods } from '../../shared/util-methods';
 
 @Component({
   selector: 'app-process-unit-card',
@@ -8,22 +9,33 @@ import { ProcessUnit, ProcessUnitStateEnum } from '../../api';
     <div class="rounded-md shadow-md cursor-pointer bg-vscode-backgroundgrey hover:brightness-95">
       <div class="px-5 py-3 text-xl bg-vscode-mediumblue text-white rounded-t-md flex justify-between">
         <div>{{processUnit?.name}}</div>
-        <div class="ml-2 codicon !text-xl" [class.codicon-play-circle]="processUnit?.state?.state === ProcessUnitStateEnum.InProgress"
-             [class.codicon-pass]="processUnit?.state?.state === ProcessUnitStateEnum.Ready"
-             [class.codicon-circle-slash]="processUnit?.state?.state === ProcessUnitStateEnum.NotOnline"
-        ></div>
+        <div class="ml-2 codicon !text-xl" [ngClass]="statusIcon"></div>
       </div>
       <div class="px-5 py-4 ">
         <div>State: {{processUnit?.state?.state | processUnitState}}</div>
         <div>Location: {{processUnit?.location}}</div>
         <div>Runtime: {{processUnit?.runtime_msec | formatDurationMsec}}</div>
       </div>
-
-
     </div>
   `,
 })
 export class ProcessUnitCardComponent {
   @Input() processUnit?: ProcessUnit;
   protected readonly ProcessUnitStateEnum = ProcessUnitStateEnum;
+
+  get statusIcon() {
+    const state = this.processUnit?.state?.state;
+    switch(state) {
+      case undefined:
+        return '';
+      case ProcessUnitStateEnum.InProgress:
+        return 'codicon-play-circle';
+      case ProcessUnitStateEnum.Ready:
+        return 'codicon-pass';
+      case ProcessUnitStateEnum.NotOnline:
+        return 'codicon-circle-slash';
+      default:
+        return UtilMethods.assertNever(state);
+    }
+  }
 }
