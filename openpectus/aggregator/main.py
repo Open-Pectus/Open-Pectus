25be54c07c4412f1375c68391ec2855f51cef498
jhk-mjolner <jhk@mjolner.dk>
2023-05-23 10:18:05 +0200

    Made aggregator docker container be builable and have it serve the frontend.

--- openpectus/aggregator/main.py
+++ openpectus/aggregator/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 
-from aggregator.routers.batch_job import router as batch_job_router
-from aggregator.routers.process_unit import router as process_unit_router
+from routers import batch_job, process_unit
+from spa import SinglePageApplication
+
 
 # TODO
 # - add lsp thingys
@@ -34,5 +35,6 @@ title = "Pectus Aggregator"
 prefix = "/api"
 print(f"*** {title} ***")
 app = FastAPI(title=title, generate_unique_id_function=custom_generate_unique_id)
-app.include_router(process_unit_router, prefix=prefix)
-app.include_router(batch_job_router, prefix=prefix)
+app.include_router(process_unit.router, prefix=prefix)
+app.include_router(batch_job.router, prefix=prefix)
+app.mount("/", SinglePageApplication(directory='frontend-dist'))
