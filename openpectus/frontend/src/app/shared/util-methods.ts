564dffdb87da73968b58a30f8537fae040fb59e9
jhk-mjolner <jhk@mjolner.dk>
2023-06-13 15:35:05 +0200

    More advanced plot showing 2 graphs with axes.

--- openpectus/frontend/src/app/shared/util-methods.ts
+++ openpectus/frontend/src/app/shared/util-methods.ts
@@ -1,4 +1,8 @@
 export class UtilMethods {
+  static isNotNullOrUndefined<T>(value: T | null | undefined): value is T {
+    return value !== null && value !== undefined;
+  }
+
   static assertNever(x: never): never {
     throw Error(`${x} was not handled`);
   }
