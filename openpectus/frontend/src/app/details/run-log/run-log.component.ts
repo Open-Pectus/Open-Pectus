035e8c5b116f1a9850c34e0d766cbc9fe57200b1
jhk-mjolner <jhk@mjolner.dk>
2023-06-21 10:40:23 +0200

    display message with empty run log.

--- openpectus/frontend/src/app/details/run-log/run-log.component.ts
+++ openpectus/frontend/src/app/details/run-log/run-log.component.ts
@@ -17,6 +17,9 @@ import { DetailsSelectors } from '../ngrx/details.selectors';
         </div>
         <app-run-log-line *ngFor="let runLogLine of runLog.lines; let index = index" [runLogLine]="runLogLine" [rowIndex]="index"
                           [gridFormat]="gridFormat | ngrxPush" [additionalColumns]="runLog.additional_columns"></app-run-log-line>
+        <p class="text-center p-2 font-semibold" *ngIf="runLog.lines.length === 0">
+          No Run Log available.
+        </p>
       </div>
     </app-collapsible-element>
   `,
