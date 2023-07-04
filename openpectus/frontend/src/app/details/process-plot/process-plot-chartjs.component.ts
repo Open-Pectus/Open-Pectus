76ed707089d0ef303e02b9689177da0ac61ee01a
jhk-mjolner <jhk@mjolner.dk>
2023-07-04 09:51:44 +0200

    Installed chartjs and very initial call to create graph

--- openpectus/frontend/src/app/details/process-plot/process-plot-chartjs.component.ts
+++ openpectus/frontend/src/app/details/process-plot/process-plot-chartjs.component.ts
@@ -1,6 +1,9 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subject } from 'rxjs';
+import { Chart } from 'chart.js';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { PlotConfiguration, ProcessValue } from '../../api';
+import { UtilMethods } from '../../shared/util-methods';
 import { DetailsSelectors } from '../ngrx/details.selectors';
 import { ProcessPlotActions } from './ngrx/process-plot.actions';
 import { ProcessPlotSelectors } from './ngrx/process-plot.selectors';
@@ -10,15 +13,16 @@ import { ProcessPlotSelectors } from './ngrx/process-plot.selectors';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <app-collapsible-element [name]="'Process Plot'" [heightResizable]="true" [contentHeight]="400" (collapseStateChanged)="collapsed = $event">
-      <div content class="h-full" #plot *ngIf="!collapsed"></div>
+      <canvas content class="h-full" #plot *ngIf="!collapsed"></canvas>
     </app-collapsible-element>`,
 })
 export class ProcessPlotChartjsComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild('plot', {static: false}) plotElement?: ElementRef<HTMLDivElement>;
+  @ViewChild('plot', {static: false}) plotElement?: ElementRef<HTMLCanvasElement>;
   protected collapsed = false;
   private processValuesLog = this.store.select(DetailsSelectors.processValuesLog);
   private plotConfiguration = this.store.select(ProcessPlotSelectors.plotConfiguration);
   private componentDestroyed = new Subject<void>();
+  private chart?: Chart;
 
   constructor(private store: Store) {}
 
@@ -27,12 +31,26 @@ export class ProcessPlotChartjsComponent implements OnInit, AfterViewInit, OnDes
   }
 
   ngAfterViewInit() {
-    if(this.plotElement === undefined) throw Error('missing plot dom element!');
-    const element = this.plotElement?.nativeElement;
-
+    this.plotConfiguration.pipe(filter(UtilMethods.isNotNullOrUndefined), takeUntil(this.componentDestroyed)).subscribe(plotConfiguration => {
+      this.processValuesLog.pipe(takeUntil(this.componentDestroyed)).subscribe(processValueLog => {
+        this.setupChart(plotConfiguration, processValueLog);
+      });
+    });
   }
 
   ngOnDestroy() {
     this.componentDestroyed.next();
+  }
+
+  private setupChart(plotConfiguration: PlotConfiguration, processValueLog: ProcessValue[][]) {
+    if(this.plotElement === undefined) throw Error('missing plot dom element!');
+    const element = this.plotElement?.nativeElement;
+    this.chart = new Chart(element, {
+      type: 'line',
+      data: {
+        labels: [],
+        datasets: [],
+      },
+    });
   }
 }
