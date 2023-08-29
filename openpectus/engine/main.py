f07509198d67869b0ed6d9f35f50593ffc95c18c
mpo@mjolner.dk <mpo@mjolner.dk>
2023-08-29 08:35:26 +0200

    Fix main to use uod builder

--- openpectus/engine/main.py
+++ openpectus/engine/main.py
@@ -9,10 +9,10 @@ from typing_extensions import override
 import httpx
 
 from openpectus.protocol.engine import Client, create_client
-from openpectus.engine.eng import ExecutionEngine, EngineCommand
+from openpectus.engine.eng import ExecutionEngine, CommandRequest
 from openpectus.engine.hardware import HardwareLayerBase, Register, RegisterDirection
 from openpectus.lang.exec import tags
-from openpectus.lang.exec.uod import UnitOperationDefinitionBase, UodCommand
+from openpectus.lang.exec.uod import UnitOperationDefinitionBase, UodBuilder, UodCommand
 from openpectus.lang.exec import readings as R
 import openpectus.protocol.messages as M
 
@@ -30,64 +30,32 @@ def get_args():
     return parser.parse_args()
 
 
-class DemoHW(HardwareLayerBase):
-    def __init__(self) -> None:
-        super().__init__()
-        self.register_values: Dict[str, Any] = {}
+def create_demo_uod() -> UnitOperationDefinitionBase:
 
-    @override
-    def read(self, r: Register) -> Any:
-        if r.name in self.registers.keys() and r.name in self.register_values.keys():
-            return self.register_values[r.name]
-        return None
+    def reset(cmd: UodCommand, args: List[Any]) -> None:
+        if cmd._exec_iterations == 0:
+            cmd.context.tags.get("Reset").set_value("Reset")
+        elif cmd._exec_iterations == 5:
+            cmd.context.tags.get("Reset").set_value("N/A")
+            cmd.set_complete()
 
-    @override
-    def write(self, value: Any, r: Register):
-        if r.name in self.registers.keys():
-            self.register_values[r.name] = value
-
-
-class DemoUod(UnitOperationDefinitionBase):
-    def define(self):
-        self.define_instrument("TestUod")
-        self.define_hardware_layer(DemoHW())
-
-        self.define_register("FT01", RegisterDirection.Both, path='Objects;2:System;2:FT01')
-        self.define_register("Reset", RegisterDirection.Both, path='Objects;2:System;2:RESET',
-                             from_tag=lambda x: 1 if x == 'Reset' else 0,
-                             to_tag=lambda x: "Reset" if x == 1 else "N/A")
-
-        self.define_tag(tags.Reading("FT01", "L/h"))
-        self.define_tag(tags.Select("Reset", value="N/A", unit=None, choices=['Reset', "N/A"]))
-
-        self.define_reading(R.Reading(label="Run time"))
-        self.define_reading(R.Reading(label="FT01"))
-        self.define_reading(R.Reading(label="Reset"))
-
-        # start = time.time()
-
-        # def x():
-        #     elapsed = time.time() - start
-        #     return elapsed
-
-        # self.tags["FT01"].get_value = x
-
-        self.define_command(DemoResetCommand())
-
-
-class DemoResetCommand(UodCommand):
-    def __init__(self) -> None:
-        super().__init__()
-        self.name = "Reset"
-        self.is_complete = False
-
-    def execute(self, args: List[Any], uod: UnitOperationDefinitionBase) -> None:
-        if self.iterations == 0:
-            uod.tags.get("Reset").set_value("Reset")
-        elif self.iterations == 5:
-            uod.tags.get("Reset").set_value("N/A")
-            self.is_complete = True
-            self.iterations = 0
+    return (
+        UodBuilder()
+        .with_instrument("DemoUod")
+        .with_no_hardware()
+        .with_hardware_register("FT01", RegisterDirection.Both, path='Objects;2:System;2:FT01')
+        .with_hardware_register("Reset", RegisterDirection.Both, path='Objects;2:System;2:RESET',
+                                from_tag=lambda x: 1 if x == 'Reset' else 0,
+                                to_tag=lambda x: "Reset" if x == 1 else "N/A")
+        .with_new_system_tags()
+        .with_tag(tags.Reading("FT01", "L/h"))
+        .with_tag(tags.Select("Reset", value="N/A", unit=None, choices=['Reset', "N/A"]))
+        .with_command(UodCommand.builder().with_name("Reset").with_exec_fn(reset))
+        .with_process_value(R.Reading(label="Run time"))
+        .with_process_value(R.Reading(label="FT01"))
+        .with_process_value(R.Reading(label="Reset"))
+        .build()
+    )
 
 
 class EngineRunner:
@@ -222,10 +190,11 @@ class WebSocketRPCEngineRunner(EngineRunner):
 
         # as side effect, start engine on first START command
         # TODO verify this is intended behavior
+        # TODO - move this into ExecutionEngine
         if not self.engine.is_running() and msg.name.upper() == "START":
             self.engine.run()
 
-        cmd = EngineCommand(name=msg.name, args=msg.arguments)
+        cmd = CommandRequest(name=msg.name, args=msg.arguments)
         self.engine.cmd_queue.put_nowait(cmd)
         return M.SuccessMessage()
 
@@ -237,7 +206,7 @@ class WebSocketRPCEngineRunner(EngineRunner):
 async def async_main(args):
     uod = None
     if args.uod == 'DemoUod':
-        uod = DemoUod()
+        uod = create_demo_uod()
     if uod is None:
         raise ValueError("Uod not configured")
 
@@ -262,15 +231,15 @@ async def async_main(args):
                 time.sleep(.1)
 
                 if e._tick_time > secs_start:
-                    e.cmd_queue.put(EngineCommand("START"))
+                    e.cmd_queue.put(CommandRequest("START"))
                     secs_start = never
 
                 if e._tick_time > secs_reset:
-                    e.cmd_queue.put(EngineCommand("RESET"))
+                    e.cmd_queue.put(CommandRequest("RESET"))
                     secs_reset = never
 
                 if e._tick_time > secs_stop:
-                    e.cmd_queue.put(EngineCommand("STOP"))
+                    e.cmd_queue.put(CommandRequest("STOP"))
                     secs_stop = never
 
         except KeyboardInterrupt:
