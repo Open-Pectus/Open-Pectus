c918b2d4f5728b16a2a056653d450b94bcab2950
jhk-mjolner <jhk@mjolner.dk>
2023-09-19 10:03:35 +0200

    Ensure observable isn't leaked in auth guard.

--- openpectus/frontend/src/app/auth/auth.guard.ts
+++ openpectus/frontend/src/app/auth/auth.guard.ts
@@ -2,13 +2,14 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
-import { mergeMap, of } from 'rxjs';
+import { mergeMap, of, take } from 'rxjs';
 import { AppSelectors } from '../ngrx/app.selectors';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authIsEnabled = inject(Store).select(AppSelectors.authIsEnabled);
   const autoLoginGuard = inject(AutoLoginPartialRoutesGuard);
   return authIsEnabled.pipe(
+    take(1),
     mergeMap(authIsEnabled => {
       if(authIsEnabled) return autoLoginGuard.canActivate(route, state);
       return of(true);
