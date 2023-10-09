214f9e469055fc649212c29c0b8ce59b77754c2f
jhk-mjolner <jhk@mjolner.dk>
2023-10-09 14:48:17 +0200

    Improving commands layout in mobile.

--- openpectus/frontend/src/app/top-bar.component.ts
+++ openpectus/frontend/src/app/top-bar.component.ts
@@ -6,9 +6,9 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
   selector: 'app-top-bar',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="w-full h-24 md:h-14 flex items-center justify-between px-4 bg-sky-900 text-white relative">
+    <div class="w-full flex items-center justify-between px-4 bg-sky-900 text-white relative">
       <app-msw-enablement class="flex-1"></app-msw-enablement>
-      <button class="text-3xl font-bold mx-4" (click)="navigateToRoot()">Open Pectus</button>
+      <button class="text-3xl font-bold mx-4 my-2.5" (click)="navigateToRoot()">Open Pectus</button>
       <ng-container *ngrxLet="oidcSecurityService.userData$ as userData">
         <div class="flex gap-4 items-center flex-1 justify-end">
           <p>{{userData.userData?.name ?? 'Anonymous'}}</p>
