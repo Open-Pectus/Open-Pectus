d6564285b13a2022f3141490d9c1c11a881aaf49
jhk-mjolner <jhk@mjolner.dk>
2023-10-23 12:18:59 +0200

    Some cleanup

--- openpectus/frontend/src/app/details/run-log/run-log-line/run-log-line-progress.component.ts
+++ openpectus/frontend/src/app/details/run-log/run-log-line/run-log-line-progress.component.ts
@@ -4,8 +4,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   selector: 'app-run-log-line-progress',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <div class="border-slate-400 bg-slate-100 border rounded-md overflow-hidden w-28 h-full flex items-center relative"
-         [title]="title">
+    <div class="border-slate-400 bg-slate-100 border rounded-md overflow-hidden w-28 h-full flex items-center relative">
       <div class="bg-sky-600 saturate-50 h-full overflow-visible flex items-center"
            [style.width.%]="value * 100" *ngIf="value !== undefined">
       </div>
@@ -15,27 +14,9 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
             [class.left-0]="value >= .5" [class.right-0]="value < .5"
             [class.text-white]="value >= .5" [class.text-black]="value < .5"
             *ngIf="value !== undefined">{{value * 100 | number:'1.1-1'}} %</span>
-
     </div>
   `,
-  styles: [
-    `@keyframes pingpong {
-       0% {
-         transform: translateX(0);
-         left: 0;
-       }
-       100% {
-         transform: translateX(-100%);
-         left: 100%;
-       }
-     }`,
-  ],
 })
 export class RunLogLineProgressComponent {
   @Input() value?: number;
-
-  get title() {
-    if(this.value === undefined) return 'no value';
-    return `${this.value * 100} %`;
-  }
 }
