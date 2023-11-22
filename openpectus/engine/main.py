de3a52abc70c63d89318cd804d5d0b7781826566
jhk-mjolner <jhk@mjolner.dk>
2023-11-22 13:43:01 +0100

    Pulled demo uod creation out into own file

--- openpectus/engine/main.py
+++ openpectus/engine/main.py
@@ -1,15 +1,12 @@
 import asyncio
 import logging
 from argparse import ArgumentParser
-from typing import Any, List
 
 from openpectus import log_setup_colorlog
+from openpectus.engine.demo_uod import create_demo_uod
 from openpectus.engine.engine import Engine
-from openpectus.engine.engine_reporter import EngineReporter
-from openpectus.engine.hardware import RegisterDirection
 from openpectus.engine.engine_message_handlers import EngineMessageHandlers
-from openpectus.lang.exec import tags, readings as R
-from openpectus.lang.exec.uod import UnitOperationDefinitionBase, UodBuilder, UodCommand
+from openpectus.engine.engine_reporter import EngineReporter
 from protocol.engine_dispatcher import EngineDispatcher
 
 log_setup_colorlog()
@@ -28,35 +25,6 @@ def get_args():
                         help="Aggregator websocket port number. Default is 9800")
     parser.add_argument("-uod", "--uod", required=False, default="DemoUod", help="The UOD to use")
     return parser.parse_args()
-
-
-def create_demo_uod() -> UnitOperationDefinitionBase:
-    def reset(cmd: UodCommand, args: List[Any]) -> None:
-        count = cmd.get_iteration_count()
-        if count == 0:
-            cmd.context.tags.get("Reset").set_value("Reset")
-        elif count == 4:
-            cmd.context.tags.get("Reset").set_value("N/A")
-            cmd.set_complete()
-
-    return (
-        UodBuilder()
-        .with_instrument("DemoUod")
-        .with_no_hardware()
-        .with_hardware_register("FT01", RegisterDirection.Both, path='Objects;2:System;2:FT01')
-        .with_hardware_register("Reset", RegisterDirection.Both, path='Objects;2:System;2:RESET',
-                                from_tag=lambda x: 1 if x == 'Reset' else 0,
-                                to_tag=lambda x: "Reset" if x == 1 else "N/A")
-        .with_new_system_tags()
-        .with_tag(tags.Reading("FT01", "L/h"))
-        .with_tag(tags.Select("Reset", value="N/A", unit=None, choices=['Reset', "N/A"]))
-        .with_command(UodCommand.builder().with_name("Reset").with_exec_fn(reset))
-        .with_process_value(R.Reading(label="Run Time"))
-        .with_process_value(R.Reading(label="FT01"))
-        .with_process_value(R.Reading(label="Reset"))
-        .with_process_value(R.Reading(label="System State"))
-        .build()
-    )
 
 
 async def async_main(args):
