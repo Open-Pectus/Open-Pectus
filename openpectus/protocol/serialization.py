c0727793aee8da03c62310e7a7ab3bf884fa8eb9
mpo@mjolner.dk <mpo@mjolner.dk>
2023-12-11 12:01:51 +0100

    Improved error handling Added namespace support in serialization Moved serialization tests  to own test class

--- openpectus/protocol/serialization.py
+++ openpectus/protocol/serialization.py
@@ -4,13 +4,18 @@ from typing import Any
 
 import openpectus.protocol.aggregator_messages as AM
 import openpectus.protocol.engine_messages as EM
+from openpectus.protocol.exceptions import ProtocolDeserializationException, ProtocolSerializationException
 import openpectus.protocol.messages as M
+
+_message_namespaces = [AM, EM, M]
+_message_namespace_names = [ns.__name__ for ns in _message_namespaces]
 
 
 def serialize(msg: M.MessageBase) -> dict[str, Any]:
     """ Serialize a protocol message in a round-trippable fashion. """
     json_dict = msg.dict()
-    json_dict["_type"] = type(msg).__name__
+    json_dict["_type"] = type(msg).__qualname__
+    json_dict["_ns"] = msg.__module__
     return json_dict
 
 
@@ -19,11 +24,17 @@ def deserialize(json_dict: dict[str, Any]) -> M.MessageBase:
 
     Supports messages serialized to dict by the serialize() function.
     """
-    if "_type" not in json_dict.keys():
-        raise ValueError("Deserialization error. Key '_type' missing.")
-    message_type = json_dict["_type"]
-    cls = getattr(EM, message_type, getattr(AM, message_type, getattr(M, message_type, None)))
-    assert not isinstance(cls, NoneType), f"Class name {cls} is not defined in any of the protocol message namespaces."
-    msg = cls(**json_dict)
-    assert isinstance(msg, M.MessageBase)
-    return msg
+    try:
+        if "_type" not in json_dict.keys() or "_ns" not in json_dict.keys():
+            raise ValueError("Deserialization error. Key '_type' or '_ns' missing.")
+        message_type, module_name = json_dict["_type"], json_dict["_ns"]
+        if module_name not in _message_namespace_names:
+            raise ValueError(f"Message module name '{module_name}' is not a valid protocol message namespace.")
+        ns = _message_namespaces[_message_namespace_names.index(module_name)]
+        cls = getattr(ns, message_type, None)
+        assert not isinstance(cls, NoneType), f"Class name {cls} is not defined in namespace '{module_name}'."
+        msg = cls(**json_dict)
+        assert isinstance(msg, M.MessageBase)
+        return msg
+    except Exception as ex:
+        raise ProtocolDeserializationException(f"Deserialization protocol error: {ex}")
--- openpectus/test/protocol/test_aggregator_engine.py
+++ openpectus/test/protocol/test_aggregator_engine.py
@@ -5,17 +5,13 @@ import unittest
 import httpx
 import openpectus.aggregator.deps as agg_deps
 import openpectus.protocol.aggregator_messages as AM
-import openpectus.protocol.engine_messages as EM
 import openpectus.protocol.messages as Msg
-import openpectus.protocol.models as Mdl
 from fastapi import Depends, FastAPI
 from fastapi.responses import Response
 from fastapi_websocket_pubsub import PubSubEndpoint
 from fastapi_websocket_rpc.logger import get_logger
 from openpectus.aggregator.aggregator import Aggregator
-from openpectus.protocol.aggregator_dispatcher import AggregatorDispatcher
 from openpectus.protocol.dispatch_interface import AGGREGATOR_RPC_WS_PATH, AGGREGATOR_HEALTH_PATH
-from openpectus.protocol.serialization import serialize, deserialize
 
 logging.basicConfig()
 logger = get_logger("Test")
@@ -363,37 +359,6 @@ def make_server_print_channels():
 #         # self.assertEqual(tag.value, "bar")  # type: ignore
 #
 #         await client.disconnect_wait_async()
-
-
-class SerializationTest(unittest.TestCase):
-    def test_serialization_RegisterEngineMsg(self):
-        reg = EM.RegisterEngineMsg(computer_name="foo", uod_name="bar")
-        reg_s = serialize(reg)
-        self.assertIsNotNone(reg_s)
-
-    def test_round_trip_RegisterEngineMsg(self):
-        reg = EM.RegisterEngineMsg(computer_name="foo", uod_name="bar")
-        reg_s = serialize(reg)
-        reg_d = deserialize(reg_s)
-        self.assertIsNotNone(reg_d)
-        self.assertIsInstance(reg_d, EM.RegisterEngineMsg)
-        self.assertEqual(reg.computer_name, reg_d.computer_name)  # type: ignore
-        self.assertEqual(reg.uod_name, reg_d.uod_name)  # type: ignore
-
-    def test_serialization_TagsUpdatedMsg(self):
-        tu = EM.TagsUpdatedMsg(tags=[Mdl.TagValue(name="foo", value="bar", value_unit="m")])
-        tu_s = serialize(tu)
-        self.assertIsNotNone(tu_s)
-
-    def test_round_trip_TagsUpdatedMsg(self):
-        tu = EM.TagsUpdatedMsg(tags=[Mdl.TagValue(name="foo", value="bar", value_unit=None)])
-        tu_s = serialize(tu)
-        self.assertIsNotNone(tu_s)
-
-        tu_d = deserialize(tu_s)
-        self.assertIsNotNone(tu_d)
-        self.assertIsInstance(tu_d, EM.TagsUpdatedMsg)
-        self.assertEqual(tu_d.tags[0].name, tu.tags[0].name)  # type: ignore
 
 
 if __name__ == "__main__":
