653f364b73239424b116ded2efbdee17b4a657c2
mpo@mjolner.dk <mpo@mjolner.dk>
2024-06-24 15:29:52 +0200

    Improve test

--- openpectus/test/protocol/test_serialization.py
+++ openpectus/test/protocol/test_serialization.py
@@ -21,17 +21,17 @@ class SerializationTest(unittest.TestCase):
         reg_s = serialize(reg)
         reg_d = deserialize(reg_s)
         self.assertIsNotNone(reg_d)
-        self.assertIsInstance(reg_d, EM.RegisterEngineMsg)
-        self.assertEqual(reg.computer_name, reg_d.computer_name)  # type: ignore
-        self.assertEqual(reg.uod_name, reg_d.uod_name)  # type: ignore
+        assert isinstance(reg_d, EM.RegisterEngineMsg)
+        self.assertEqual(reg.computer_name, reg_d.computer_name)
+        self.assertEqual(reg.uod_name, reg_d.uod_name)
 
     def test_round_trip_AM_MethodMsg(self):
         am_method = AM.MethodMsg(method=Mdl.Method(lines=[Mdl.MethodLine(id='87', content='Foo')]))
         reg_s = serialize(am_method)
         reg_d = deserialize(reg_s)
         self.assertIsNotNone(reg_d)
-        self.assertIsInstance(reg_d, AM.MethodMsg)
-        self.assertEqual(am_method.method, reg_d.method)  # type: ignore
+        assert isinstance(reg_d, AM.MethodMsg)
+        self.assertEqual(am_method.method, reg_d.method)
 
     def test_serialization_TagsUpdatedMsg(self):
         tu = EM.TagsUpdatedMsg(tags=[Mdl.TagValue(name="foo", tick_time=0, value="bar", value_unit="m")])
@@ -45,8 +45,8 @@ class SerializationTest(unittest.TestCase):
 
         tu_d = deserialize(tu_s)
         self.assertIsNotNone(tu_d)
-        self.assertIsInstance(tu_d, EM.TagsUpdatedMsg)
-        self.assertEqual(tu_d.tags[0].name, tu.tags[0].name)  # type: ignore
+        assert isinstance(tu_d, EM.TagsUpdatedMsg)
+        self.assertEqual(tu_d.tags[0].name, tu.tags[0].name)
 
 
 if __name__ == "__main__":
