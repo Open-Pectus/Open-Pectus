446a301901e38ae9c7c037982ae402898e895225
mpo@mjolner.dk <mpo@mjolner.dk>
2024-08-02 11:43:18 +0200

    Remove version end point from schema

--- openpectus/aggregator/routers/version.py
+++ openpectus/aggregator/routers/version.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from openpectus import __version__
 
-router = APIRouter(tags=["version"])
+router = APIRouter(tags=["version"], include_in_schema=False)
 
 @router.get("/version")
 def get_version():
