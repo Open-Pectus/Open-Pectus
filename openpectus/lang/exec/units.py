0f2ed00f6b757767d86dce0eabf637e4cb16b1a0
mpo@mjolner.dk <mpo@mjolner.dk>
2024-09-02 09:33:43 +0200

    Clean up units module

--- openpectus/lang/exec/units.py
+++ openpectus/lang/exec/units.py
@@ -1,21 +1,17 @@
 from __future__ import annotations
 import logging
-from typing import Any, Literal
 
 import pint
 from pint import UnitRegistry, Quantity
-from pint.facets.plain import PlainQuantity
 
 
 ureg = UnitRegistry(cache_folder="./pint-cache")
 Q_ = Quantity
-
-QuantityType = pint.Quantity | PlainQuantity[Any]
 logger = logging.getLogger(__name__)
 
 # TODO add temperatures degF, degK, CV is not a known pint unit
-
-# defines all the units we accept
+# https://en.wikipedia.org/wiki/International_System_of_Units
+# defines all the units we accept, each with their quantity name (dimension in pint).
 QUANTITY_UNIT_MAP = {
     'time': ['s', 'min', 'hour', 'ms'],     # SI quantity
     'length': ['m', 'cm'],                  # SI quantity
@@ -27,7 +23,6 @@ QUANTITY_UNIT_MAP = {
     'percentage': ['%']                     # Custom quantity
 }
 
-# defines a mapping between the supported quantity names and pint dimensions
 QUANTITY_PINT_MAP: dict[str, str | None] = {
     'time': '[time]',
     'length': '[length]',
@@ -37,24 +32,17 @@ QUANTITY_PINT_MAP: dict[str, str | None] = {
     'temperature': None,
     'amount_of_substance': '[substance]',
     'percentage': None
-
 }
+""" Map quantity names to pint dimensions or None if not a pint dimension. """
 
-pint_unit_map = {
-    '[length]': ['m', 'cm'],
-    '[mass]': ['kg', 'g'],
-    '[time]': ['s', 'min', 'h', 'ms'],
-    'flow': ['L/h', 'L/min', 'L/d'],  # Pint parses L/m as liter/meter
-    '[length] ** 3': ['L', 'mL'],
-    '[substance]': ['mol']
-}
-
-pint_unit_dims = {
+QUANTITY_PINT_MULTIDIM_MAP = {
     'liter / hour': 'flow',
     'liter / minute': 'flow',
     'liter / day': 'flow'
 }
+""" Map pint units with multi-dimensionality to quantity name"""
 
+# build list of all supported units
 _supported_units: list[None | str] = [None]
 for v in QUANTITY_UNIT_MAP.values():
     _supported_units.extend(v)
@@ -75,21 +63,22 @@ def is_supported_unit(unit: str) -> bool:
 def get_volume_units():
     return QUANTITY_UNIT_MAP['volume']
 
-def as_quantity(value, unit) -> pint.Quantity:
-    # TODO - non-pint units have no pint.Quantity representation - so how do we compare them?
-    qn = get_unit_quantity_name(unit)
-    pn = QUANTITY_PINT_MAP[qn]
-    if pn is None:
-        raise NotImplementedError(f"Unit {unit} is not a pint unit so how do we get the quantity?")
-    return pint.Quantity(value, unit)  # type: ignore
-
 def get_unit_quantity_name(unit: str) -> str:
+    """ For a supported unit that is not None, return its quantity name. Raises ValueError if the unit is not supported. """
     for key in QUANTITY_UNIT_MAP.keys():
         if unit in QUANTITY_UNIT_MAP[key]:
             return key
     raise ValueError(f"Invalid unit: '{unit}'")
 
+def _get_quantity_name_for_pint_dim(pint_dimension: str) -> str | None:
+    for key in QUANTITY_PINT_MAP.keys():
+        vals = QUANTITY_PINT_MAP[key]
+        if vals is not None:
+            if pint_dimension in vals:
+                return key
+
 def get_compatible_unit_names(unit: str | None) -> list[str]:
+    """ For a supported unit, return its compatible units. If the unit is not supported, a ValueError is raised. """
     if unit is None:
         return [""]
 
@@ -98,7 +87,7 @@ def get_compatible_unit_names(unit: str | None) -> list[str]:
 
     pint_mapping = QUANTITY_PINT_MAP[quantity_name]
     if pint_mapping is None:
-        # non-pint unit - return all units of this quantity name
+        # non-pint unit, return all units of this quantity name
         return QUANTITY_UNIT_MAP[quantity_name]
 
     pu = _get_pint_unit(unit)
@@ -111,21 +100,23 @@ def get_compatible_unit_names(unit: str | None) -> list[str]:
     else:
         dims = pu.dimensionality
         if len(dims) == 1:
-            unit_names = pint_unit_map.get(str(dims))
-            if unit_names is None:
-                raise NotImplementedError(f"Unit {pu} with dimensionality {dims} has no defined compatible units")
-            else:
-                return unit_names
+            quantity_name = _get_quantity_name_for_pint_dim(str(dims))
+            if quantity_name is None:
+                raise ValueError(f"Unit {pu} with dimensionality {dims} has no defined compatible units")
+            return QUANTITY_UNIT_MAP[quantity_name]
         else:
-            dim_name = pint_unit_dims.get(str(pu))
-            if dim_name is None:
-                raise NotImplementedError(f"Unit '{pu}' has no defined compatible units")
-            unit_names = pint_unit_map.get(dim_name)
-            if unit_names is None:
-                raise NotImplementedError(f"Unit '{pu}' has no defined compatible units for dimension {dim_name}")
-            return unit_names
+            quantity_name = QUANTITY_PINT_MULTIDIM_MAP.get(str(pu))
+            if quantity_name is None:
+                raise NotImplementedError(f"Pint unit '{pu}' has no defined quantity name")
+            return QUANTITY_UNIT_MAP[quantity_name]
 
-def are_comparable(unit_a: str | None, unit_b: str | None):
+def are_comparable(unit_a: str | None, unit_b: str | None) -> bool:
+    """ Determine whether two supported units are comparable, i.e. have the same quantity name.
+
+    For example, 'cm' and 'm' are comparable because they have the same quantity name, 'length'.
+
+    If one or both units are not supported, raise ValueError.
+    """
     if unit_a == unit_b:
         return True
     if unit_a is None or unit_b is None:
@@ -136,19 +127,13 @@ def are_comparable(unit_a: str | None, unit_b: str | None):
     result = unit_b in compatibles_a
     return result
 
-def _get_pint_unit(unit: str | None) -> pint.Unit | None:
-    if unit is None:
-        return None
-    try:
-        return pint.Unit(unit)
-    except Exception:
-        logger.error(f"Could not get pint unit for unit '{unit}'")
-        raise
-
-
 def compare_values(op: str, value_a: str, unit_a: str | None, value_b: str, unit_b: str | None) -> bool:
-    result = None
+    """ Compare two values using the provided operator (<, <=, >, >=, =, ==, !=) and return the comparison result.
 
+    If units are given, the units are checked for comparability. The units may be different as long as they are comparable.
+
+    In case of error, a ValueError is raised.
+    """
     if not are_comparable(unit_a, unit_b):
         raise ValueError(f"Cannot compare values with incompatible units '{unit_a}' and '{unit_b}'")
 
@@ -159,7 +144,8 @@ def compare_values(op: str, value_a: str, unit_a: str | None, value_b: str, unit
 
         pint_mapping = QUANTITY_PINT_MAP.get(quantity_name, None)
         if pint_mapping is None:
-            # different non-pint units cannot currently be compared
+            # different non-pint units cannot currently be compared. To support these, we must provide a conversion
+            # to the base (SI) unit that can be applied before comparison.
             raise NotImplementedError(f"Custom comparison of non-pint units {unit_a} and {unit_b} is not implemented")
         else:
             is_pint_units = True
@@ -170,7 +156,6 @@ def compare_values(op: str, value_a: str, unit_a: str | None, value_b: str, unit
             raise ValueError("Cannot compare values, first value is missing or not numeric")
         if fval_b is None:
             raise ValueError("Cannot compare values, second value is missing or not numeric")
-
         quantity_a = value_a if unit_a is None else pint.Quantity(fval_a, unit_a)
         quantity_b = value_b if unit_b is None else pint.Quantity(fval_b, unit_b)
     else:
@@ -178,14 +163,11 @@ def compare_values(op: str, value_a: str, unit_a: str | None, value_b: str, unit
         quantity_a = value_a
         quantity_b = value_b
 
-    # we can now ignore units and conversion - either they're None or equal - or they're
-    # pint units for which Quantity handles conversion automatically
+    # we can now ignore units and conversion - either they're None or equal - or they are
+    # pint units for which Quantity handles comparison automatically
 
     # infer when to do numeric vs string comparison
-    if op in ['=', '==', '!=']:
-        pass
-    else:
-        # < and > only make sence for numeric values
+    if op in ['<', '<=', '>', '>=']:  # these operators only make sence for numeric values
         if not is_pint_units:
             assert isinstance(quantity_a, str), f"quantity_a '{quantity_a}' should be a str here"
             assert isinstance(quantity_b, str), f"quantity_b '{quantity_b}' should be a str here"
@@ -220,9 +202,6 @@ def compare_values(op: str, value_a: str, unit_a: str | None, value_b: str, unit
     assert isinstance(result, bool), f"Comparison result was not type bool but {type(result)}"
     return result
 
-
-
-
 def as_float(value: str) -> float | None:
     """ Parse string value as a float and return it. If the value is not a number, return None. """
     try:
@@ -237,3 +216,12 @@ def as_int(value: str) -> int | None:
         if fval.is_integer():
             return int(fval)
     return None
+
+def _get_pint_unit(unit: str | None) -> pint.Unit | None:
+    if unit is None:
+        return None
+    try:
+        return pint.Unit(unit)
+    except Exception:
+        logger.error(f"Could not get pint unit for unit '{unit}'")
+        raise
