d610f0cf62ac1dd6c294437d43cdc67da9f5e4cb
mpo@mjolner.dk <mpo@mjolner.dk>
2024-09-10 09:43:48 +0200

    Fix bug

--- openpectus/test/engine/test_tag.py
+++ openpectus/test/engine/test_tag.py
@@ -11,10 +11,12 @@ class TagTest(unittest.TestCase):
         tag = Tag("foo")
 
         tag.format_fn = format_time_as_clock
-        _date = datetime.datetime(year=2000, month=1, day=1, hour=6, minute=46, second=3, microsecond=111)
+        _date = datetime.datetime(
+            year=2000, month=1, day=1, hour=6, minute=46, second=3, microsecond=111, tzinfo=datetime.UTC
+        )
         tag.value = _date.timestamp()
 
         t = tag.as_readonly()
 
-        self.assertNotEqual("06:46:03", t.value_formatted)
+        self.assertEqual("06:46:03", t.value_formatted)
         self.assertIsNotNone(t.value_formatted)
