778362e4427c9c3bb66768c96ef8bfd3e1490e30
jhk-mjolner <jhk@mjolner.dk>
2024-09-24 10:20:29 +0200

    Display value_formatted from processValue when available and without commands.

--- openpectus/frontend/src/app/shared/pipes/process-value.pipe.ts
+++ openpectus/frontend/src/app/shared/pipes/process-value.pipe.ts
@@ -10,14 +10,18 @@ import { UtilMethods } from '../util-methods';
 export class ProcessValuePipe implements PipeTransform {
   constructor(private decimalPipe: DecimalPipe) {}
 
-  transform(processValue: Pick<ProcessValue, 'value' | 'value_type' | 'value_unit'>
-      | ProcessValueCommandChoiceValue | ProcessValueCommandFreeTextValue | ProcessValueCommandNumberValue
-      | undefined,
+  transform(processValue: undefined |
+    (Pick<ProcessValue, 'value' | 'value_type' | 'value_unit'> | ProcessValueCommandChoiceValue | ProcessValueCommandFreeTextValue | ProcessValueCommandNumberValue)
+    & Pick<ProcessValue, 'value_formatted' | 'commands'>,
   ): string | null {
     if(processValue?.value === undefined) return null;
+
+    const hasCommands = processValue.commands !== undefined && processValue.commands.length > 0;
+    if(!hasCommands && processValue.value_formatted !== undefined) return processValue.value_formatted;
+
     const valueType = processValue.value_type;
-    if(valueType === undefined) return null;
     switch(valueType) {
+      case null:
       case 'none':
         return null;
       case 'string':
