ae3a8eb13bc394367784975065905ed62d563806
jhk-mjolner <jhk@mjolner.dk>
2024-10-15 15:22:44 +0200

    An attempt at better using space with variable row span of elements in details page.

---
+++ openpectus/frontend/src/app/details/variable-row-span.directive.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Directive, ElementRef, HostBinding, OnDestroy } from '@angular/core';
+
+@Directive({
+  selector: '[appVariableRowSpan]',
+  standalone: true,
+})
+export class VariableRowSpanDirective implements AfterViewInit, OnDestroy {
+  collapsedElementHeight = 54;
+  rowGap = 32;
+  private contentHeight = 0;
+  private observer = new ResizeObserver((entries: ResizeObserverEntry[], _: ResizeObserver) => {
+    this.contentHeight = entries.at(0)?.contentRect?.height ?? 0;
+  });
+
+  constructor(private ref: ElementRef<HTMLElement>) {}
+
+  @HostBinding('style.grid-row') get rowSpan() {
+    const span = Math.ceil((this.collapsedElementHeight + this.contentHeight) / (this.collapsedElementHeight + this.rowGap));
+    return `span ${span} / span ${span}`;
+  }
+
+  ngAfterViewInit() {
+    const contentElement = this.ref.nativeElement.querySelector('[content]');
+    if(contentElement === null) return;
+    this.observer.observe(contentElement);
+  }
+
+  ngOnDestroy() {
+    this.observer.disconnect();
+  }
+}
