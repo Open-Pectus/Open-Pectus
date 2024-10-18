cfcc698403ee670e0693f6c17ca0f8fd1296b370
jhk-mjolner <jhk@mjolner.dk>
2024-10-18 13:05:02 +0200

    Avoid a little jump after collapse/expand

--- openpectus/frontend/src/app/details/variable-row-span.directive.ts
+++ openpectus/frontend/src/app/details/variable-row-span.directive.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Directive, ElementRef, HostBinding, OnDestroy } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Directive, ElementRef, HostBinding, OnDestroy } from '@angular/core';
 
 @Directive({
   selector: '[appVariableRowSpan]',
@@ -11,16 +11,18 @@ export class VariableRowSpanDirective implements AfterViewInit, OnDestroy {
   private resizeObserver = new ResizeObserver(() => {
     const collapsibleElementHeight = this.collapsibleElement?.getBoundingClientRect().height ?? 0;
     this.rowSpan = Math.ceil(collapsibleElementHeight) + this.rowGap;
+    this.cd.markForCheck();
   });
 
-  constructor(private ref: ElementRef<HTMLElement>) {}
+  constructor(private ref: ElementRef<HTMLElement>,
+              private cd: ChangeDetectorRef) {}
 
   @HostBinding('style.grid-row') get gridRow() {
     return `span ${this.rowSpan} / span ${this.rowSpan}`;
   }
 
   ngAfterViewInit() {
-    this.collapsibleElement = this.ref.nativeElement.querySelector('app-collapsible-element') ?? undefined
+    this.collapsibleElement = this.ref.nativeElement.querySelector('app-collapsible-element') ?? undefined;
     const contentContainer = this.ref.nativeElement.querySelector('[content]')?.parentElement ?? null;
     if(contentContainer === null) return;
     this.resizeObserver.observe(contentContainer);
