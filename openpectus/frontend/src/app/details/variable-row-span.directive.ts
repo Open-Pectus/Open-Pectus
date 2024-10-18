3a43d03dbb7bb9f2abe10bb9beee7d07b26499ee
jhk-mjolner <jhk@mjolner.dk>
2024-10-18 12:58:26 +0200

    Avoid using a magic constant (measured value)

--- openpectus/frontend/src/app/details/variable-row-span.directive.ts
+++ openpectus/frontend/src/app/details/variable-row-span.directive.ts
@@ -5,27 +5,28 @@ import { AfterViewInit, Directive, ElementRef, HostBinding, OnDestroy } from '@a
   standalone: true,
 })
 export class VariableRowSpanDirective implements AfterViewInit, OnDestroy {
-  collapsedElementHeight = 54;
-  private contentHeight = 0;
-  private observer = new ResizeObserver((entries: ResizeObserverEntry[], _: ResizeObserver) => {
-    this.contentHeight = entries.at(0)?.contentRect?.height ?? 0;
+  private rowSpan = 0;
+  private collapsibleElement?: HTMLElement;
+  private readonly rowGap = 32;
+  private resizeObserver = new ResizeObserver(() => {
+    const collapsibleElementHeight = this.collapsibleElement?.getBoundingClientRect().height ?? 0;
+    this.rowSpan = Math.ceil(collapsibleElementHeight) + this.rowGap;
   });
 
   constructor(private ref: ElementRef<HTMLElement>) {}
 
-  @HostBinding('style.grid-row') get rowSpan() {
-    const myHeight = this.collapsedElementHeight + this.contentHeight;
-    const span = Math.ceil(myHeight) + 32;
-    return `span ${span} / span ${span}`;
+  @HostBinding('style.grid-row') get gridRow() {
+    return `span ${this.rowSpan} / span ${this.rowSpan}`;
   }
 
   ngAfterViewInit() {
+    this.collapsibleElement = this.ref.nativeElement.querySelector('app-collapsible-element') ?? undefined
     const contentContainer = this.ref.nativeElement.querySelector('[content]')?.parentElement ?? null;
     if(contentContainer === null) return;
-    this.observer.observe(contentContainer);
+    this.resizeObserver.observe(contentContainer);
   }
 
   ngOnDestroy() {
-    this.observer.disconnect();
+    this.resizeObserver.disconnect();
   }
 }
