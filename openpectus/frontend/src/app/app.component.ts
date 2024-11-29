b7eea2788003577f13c9719166f429ff0772ac8b
jhk-mjolner <jhk@mjolner.dk>
2024-11-29 09:39:22 +0100

    Don't try using monaco editor JSON sampleServer
    And removed a console.log

--- openpectus/frontend/src/app/app.component.ts
+++ openpectus/frontend/src/app/app.component.ts
@@ -25,7 +25,6 @@ export class AppComponent implements OnInit {
     this.oidcSecurityService.checkAuth().subscribe(
       (loginResponse) => {
         this.store.dispatch(AppActions.finishedAuthentication({isAuthenticated: loginResponse.isAuthenticated}));
-        console.log('callback authenticated', loginResponse);
       },
     );
 
