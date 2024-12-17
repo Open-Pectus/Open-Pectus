4b57e217904126580b9dbbd4c4b7170538763a18
mpo@mjolner.dk <mpo@mjolner.dk>
2024-12-17 08:47:06 +0100

    Fix warning Unsupported escape sequence

--- openpectus/lang/exec/uod.py
+++ openpectus/lang/exec/uod.py
@@ -901,7 +901,7 @@ class RegexNamedArgumentParser():
         if "option" not in self.get_named_groups():
             return []
         start = self.regex.index("|(") + len("|(")
-        end = self.regex.index("|\+)+))\s*")
+        end = self.regex.index("|\\+)+))\\s*")
         result = self.regex[start: end].split("|")
         return result
 
