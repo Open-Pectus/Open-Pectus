d3019cb8f03d3aa7f2c3abaa7c5bf7943eac248e
mpo@mjolner.dk <mpo@mjolner.dk>
2025-01-24 09:11:30 +0100

    Remove unnecessary route paths

--- openpectus/aggregator/routers/lsp.py
+++ openpectus/aggregator/routers/lsp.py
@@ -20,48 +20,10 @@ def get_registered_engine_data_or_fail(engine_id: str, agg: Aggregator) -> Mdl.E
     return engine_data
 
 
-@router.get('/tags/{engine_id}', response_model_exclude_none=True)
-def get_tags(
-        engine_id: str,
-        response: Response,
-        agg: Aggregator = Depends(agg_deps.get_aggregator)
-        ) -> list[Dto.TagDefinition]:
-    response.headers["Cache-Control"] = "no-store"
-    engine_data = get_registered_engine_data_or_fail(engine_id, agg)
-    return [
-        Dto.TagDefinition(name=t.name, unit=t.value_unit)
-        for t in engine_data.tags_info.values()
-    ]
-
-@router.get('/commands/{engine_id}', response_model_exclude_none=True)
-def get_commands(
-        engine_id: str,
-        response: Response,
-        agg: Aggregator = Depends(agg_deps.get_aggregator)
-        ) -> list[Dto.CommandDefinition]:
-    response.headers["Cache-Control"] = "no-store"
-    engine_data = get_registered_engine_data_or_fail(engine_id, agg)
-    tags_info = engine_data.tags_info.map
-    process_values: list[Dto.ProcessValue] = []
-    for tag_value in tags_info.values():
-        matching_reading = next((r for r in engine_data.readings if r.tag_name == tag_value.name), None)
-        if matching_reading is not None:
-            try:
-                cmds = command_util.create_reading_commands(tag_value, matching_reading)
-                process_values.append(Dto.ProcessValue.create_w_commands(tag_value, cmds))
-            except Exception as ex:
-                logger.error(f"Error creating commands for process value '{matching_reading.tag_name}': {ex}")
-        else:
-            process_values.append(Dto.ProcessValue.create(tag_value))
-
-    cmd_defs: list[Dto.CommandDefinition] = []
-    for p in process_values:
-        if p.commands is not None:
-            for p_cmd in p.commands:
-                cmd_defs.append(Dto.CommandDefinition(name=p_cmd.name))
-    return cmd_defs
-
-
+# changes to do
+# - maybe rename to something more generic
+# - return uod command validation as "command argument spec data" so lsp server can validate using this data rather than a local uod
+# - include "command argument spec data" for internal engine commands
 @router.get('/uod/{engine_id}', response_model_exclude_none=True)
 def get_uod_info(
         engine_id: str,
