1d31acae6b71478c603c99b6cea71e20a145ade8
Eskild Schroll-Fleischer <54531016+nneskildsf@users.noreply.github.com>
2025-01-29 15:08:13 +0100

    Import sys module

--- openpectus/lang/exec/uod.py
+++ openpectus/lang/exec/uod.py
@@ -4,6 +4,7 @@ from inspect import _ParameterKind, Parameter
 import logging
 import re
 from typing import Any, Callable, Literal, Tuple
+import sys
 
 from openpectus.engine.commands import ContextEngineCommand, CommandArgs
 from openpectus.engine.hardware import HardwareLayerBase, NullHardware, Register, RegisterDirection
