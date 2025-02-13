36d5107004c225c00e6e9b13f8e1235762099235
mpo@mjolner.dk <mpo@mjolner.dk>
2025-02-13 13:19:26 +0100

    Refactor PerformanceTimer to wrap other handlers leaving EventEmitter largely unchanged

--- openpectus/lang/exec/events.py
+++ openpectus/lang/exec/events.py
@@ -4,34 +4,11 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 import logging
+import sys
 from typing import Iterable
 import time
 
 logger = logging.getLogger(__name__)
-
-
-class PerformanceTimer:
-    t0: float
-    t1: float
-
-    def __init__(self, listener):
-        from openpectus.lang.exec.tags import Tag  # Avoid circular import by importing here.
-        self._warned = False
-        self.listener = listener
-        self.is_tag = False
-        if callable(listener) and hasattr(listener, "__self__"):
-            self.is_tag = issubclass(getattr(getattr(listener, "__self__"), "__class__"), Tag)
-
-    def __enter__(self):
-        self.t0 = time.perf_counter()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.t1 = time.perf_counter()
-        dt = self.t1-self.t0
-        if self.is_tag and not self._warned and dt > 0.01:
-            logger.warning(f"{self.listener.__name__} method for {self.listener.__self__} " +
-                           f"is slow. Execution time: {dt:0.1f} s. This warning is only issued once.")
-            self._warned = True
 
 
 class EventListener:
@@ -88,103 +65,153 @@ class EventListener:
         self.run_id = None
 
 
+class PerformanceTimer(EventListener):
+    """ Monitors execution time of event handlers and warns if they're slow. """
+    t0: float
+    t1: float
+
+    def __init__(self, listener: EventListener):
+        from openpectus.lang.exec.tags import Tag  # Avoid circular import by importing here.
+        self._warned = False
+        self._listener = listener
+        self._is_tag = isinstance(listener, Tag)
+        self._event_name: str = ""
+
+    def __enter__(self):
+        try:
+            # hack to get name of the calling method which is the event handler
+            self._event_name = sys._getframe().f_back.f_code.co_name
+        except Exception:
+            self._event_name = "(unknown)"
+        self.t0 = time.perf_counter()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.t1 = time.perf_counter()
+        dt = self.t1-self.t0
+        if self._is_tag and not self._warned and dt > 0.01:
+            logger.warning(f"{self._listener} event handler '{self._event_name}' " +
+                           f"is slow. Execution time: {dt:0.1f} s. This warning is only issued once.")
+            self._warned = True
+        self._event_name = ""
+
+    def on_engine_configured(self):
+        with self:
+            self._listener.on_engine_configured()
+
+    def _on_before_start(self, run_id: str):
+        self.run_id = run_id
+        with self:
+            self._listener._on_before_start(run_id)
+
+    def on_start(self, run_id: str):
+        with self:
+            self._listener.on_start(run_id)
+
+    def on_block_start(self, block_info: BlockInfo):
+        with self:
+            self._listener.on_block_start(block_info)
+
+    def on_block_end(self, block_info: BlockInfo, new_block_info: BlockInfo | None):
+        with self:
+            self._listener.on_block_end(block_info, new_block_info)
+
+    def on_tick(self, tick_time: float, increment_time: float):
+        with self:
+            self._listener.on_tick(tick_time, increment_time)
+
+    def on_method_end(self):
+        with self:
+            self._listener.on_method_end()
+
+    def on_stop(self):
+        with self:
+            self._listener.on_stop()
+        self.run_id = None
+
+    def on_engine_shutdown(self):
+        with self:
+            self._listener.on_engine_shutdown()
+        self.run_id = None
+
+    def __str__(self) -> str:
+        return f"PerformanceTimer(listener: {self._listener}, _warned: {self._warned}"
+
+
 class EventEmitter:
-    handle_names = [
-        "on_engine_configured",
-        "_on_before_start",
-        "on_start",
-        "on_tick",
-        "on_block_start",
-        "on_block_end",
-        "on_method_end",
-        "on_stop",
-        "on_engine_shutdown",
-    ]
     """ Emitter of EventListener events.
 
     Enables emitting execution events to a collection of listeners.
     """
     def __init__(self, listeners: Iterable[EventListener]) -> None:
-        self._listeners: list[EventListener] = list(listeners)
-        self._performance_timers = dict()
-        for listener in self._listeners:
-            for handle_name in self.handle_names:
-                handle = getattr(listener, handle_name)
-                self._performance_timers[handle] = PerformanceTimer(handle)
+        self._use_perf_timers = True
+        self._listeners: list[EventListener] = []
+        for listener in listeners:
+            self.add_listener(listener)
 
     def add_listener(self, listener: EventListener):
-        self._listeners.append(listener)
-        for handle_name in self.handle_names:
-            handle = getattr(listener, handle_name)
-            self._performance_timers[handle] = PerformanceTimer(handle)
+        if self._use_perf_timers:
+            self._listeners.append(PerformanceTimer(listener))
+        else:
+            self._listeners.append(listener)
 
     def emit_on_engine_configured(self):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_engine_configured]:
-                    listener.on_engine_configured()
+                listener.on_engine_configured()
             except Exception:
                 logger.error(f"on_engine_configured failed for listener '{listener}'", exc_info=True)
 
     def emit_on_start(self, run_id: str):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener._on_before_start]:
-                    listener._on_before_start(run_id)
+                listener._on_before_start(run_id)
             except Exception:
                 logger.error(f"on_before_start failed for listener '{listener}'", exc_info=True)
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_start]:
-                    listener.on_start(run_id)
+                listener.on_start(run_id)
             except Exception:
                 logger.error(f"on_engine_start failed for listener '{listener}'", exc_info=True)
 
     def emit_on_tick(self, tick_time: float, increment_time: float):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_tick]:
-                    listener.on_tick(tick_time, increment_time)
+                listener.on_tick(tick_time, increment_time)
             except Exception:
                 logger.error(f"on_tick failed for listener '{listener}'", exc_info=True)
 
     def emit_on_block_start(self, block_name: str, tick: int):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_block_start]:
-                    listener.on_block_start(BlockInfo(block_name, tick))
+                listener.on_block_start(BlockInfo(block_name, tick))
             except Exception:
                 logger.error(f"on_block_start failed for listener '{listener}'", exc_info=True)
 
     def emit_on_block_end(self, block_name: str, new_block_name: str, tick: int):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_block_end]:
-                    listener.on_block_end(BlockInfo(block_name, tick), BlockInfo(new_block_name, tick))
+                listener.on_block_end(BlockInfo(block_name, tick), BlockInfo(new_block_name, tick))
             except Exception:
                 logger.error(f"on_block_end failed for listener '{listener}'", exc_info=True)
 
     def emit_on_method_end(self):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_method_end]:
-                    listener.on_method_end()
+                listener.on_method_end()
             except Exception:
                 logger.error(f"on_method_end failed for listener '{listener}'", exc_info=True)
 
     def emit_on_stop(self):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_stop]:
-                    listener.on_stop()
+                listener.on_stop()
             except Exception:
                 logger.error(f"on_stop failed for listener '{str(listener)}'", exc_info=True)
 
     def emit_on_engine_shutdown(self):
         for listener in self._listeners:
             try:
-                with self._performance_timers[listener.on_engine_shutdown]:
-                    listener.on_engine_shutdown()
+                listener.on_engine_shutdown()
             except Exception:
                 logger.error(f"on_engine_shutdown failed for listener '{listener}'", exc_info=True)
 
