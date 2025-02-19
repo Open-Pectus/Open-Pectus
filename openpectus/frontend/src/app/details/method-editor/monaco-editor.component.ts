5cfc9ee62e1300f960a646ee30832e616c8cf06f
jhk-mjolner <jhk@mjolner.dk>
2025-02-19 10:43:47 +0100

    use angular.json asset glob to automatically copy the editor worker file.

--- openpectus/frontend/src/app/details/method-editor/monaco-editor.component.ts
+++ openpectus/frontend/src/app/details/method-editor/monaco-editor.component.ts
@@ -49,7 +49,7 @@ export class MonacoEditorComponent implements AfterViewInit, OnDestroy {
     useWorkerFactory({
       workerLoaders: {
         'TextEditorWorker': () => new Worker(
-          '/assets/editorWorker-es.js', {type: 'module'},
+          '/workers/editorWorker-es.js', {type: 'module'},
           // new URL('../../../assets/editorWorker-es.js', import.meta.url), {type: 'module'},
           // new URL('monaco-editor-wrapper/workers/module/editor', import.meta.url), {type: 'module'},
           // new URL('node_modules/monaco-editor-wrapper/dist/workers/editorWorker-es.js'), {type: 'module'},
