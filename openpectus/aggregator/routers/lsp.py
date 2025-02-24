92c07b073ad76443a33f0f94d79beddec07d95a1
jhk-mjolner <jhk@mjolner.dk>
2025-02-24 12:52:59 +0100

    settings frontend proxy.conf to proxy /lsp

--- openpectus/aggregator/routers/lsp.py
+++ openpectus/aggregator/routers/lsp.py
@@ -9,7 +9,7 @@ from starlette.websockets import WebSocket
 
 logger = logging.getLogger(__name__)
 router = APIRouter(tags=["lsp"], include_in_schema=False)
-proxy = ReverseWebSocketProxy(AsyncClient(), base_url="ws://localhost:2087/")
+proxy = ReverseWebSocketProxy(AsyncClient(), base_url="ws://127.0.0.1:2087/")
 
 @asynccontextmanager
 async def close_proxy_event(_: FastAPI) -> AsyncIterator[None]:
@@ -21,5 +21,4 @@ router.lifespan_context(close_proxy_event)
 
 @router.websocket("/lsp")
 async def _(websocket: WebSocket):
-    logger.info("fdas")
-    return await proxy.proxy(websocket=websocket, path="lsp")
+    return await proxy.proxy(websocket=websocket)
