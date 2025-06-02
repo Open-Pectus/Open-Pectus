5cb140d55a8ccd4501c223f0d738ded8ef91f6ea
jhk-mjolner <jhk@mjolner.dk>
2025-06-02 10:34:30 +0200

    monaco editor disposal improvements

--- frontend/src/app/details/method-editor/monaco-editor.component.ts
+++ frontend/src/app/details/method-editor/monaco-editor.component.ts
@@ -2,11 +2,11 @@ import {
   AfterViewInit,
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
   ElementRef,
   EventEmitter,
   Injector,
   input,
-  OnDestroy,
   Output,
   runInInjectionContext,
   ViewChild,
@@ -15,7 +15,7 @@ import {
 import '@codingame/monaco-vscode-theme-defaults-default-extension';
 import { editor as MonacoEditor } from '@codingame/monaco-vscode-editor-api';
 import { MonacoEditorLanguageClientWrapper } from 'monaco-editor-wrapper';
-import { Observable, Subject, take } from 'rxjs';
+import { Observable } from 'rxjs';
 import { MonacoEditorBehaviours } from './monaco-editor-behaviours';
 import { MonacoWrapperConfig } from './monaco-wrapper-config';
 
@@ -28,7 +28,7 @@ import { MonacoWrapperConfig } from './monaco-wrapper-config';
     <div #editor class="w-full h-full"></div>
   `,
 })
-export class MonacoEditorComponent implements AfterViewInit, OnDestroy {
+export class MonacoEditorComponent implements AfterViewInit {
   editorSizeChange = input.required<Observable<void>>();
   unitId = input<string>();
   editorContent = input<string>();
@@ -36,20 +36,15 @@ export class MonacoEditorComponent implements AfterViewInit, OnDestroy {
   @Output() editorContentChanged = new EventEmitter<string[]>();
   @Output() editorIsReady = new EventEmitter<MonacoEditor.IStandaloneCodeEditor>();
   @ViewChild('editor', {static: true}) editorElement!: ElementRef<HTMLDivElement>;
-  private componentDestroyed = new Subject<void>();
   private wrapper = new MonacoEditorLanguageClientWrapper();
 
-  constructor(private injector: Injector) {}
+  constructor(private injector: Injector, private destroyRef: DestroyRef) {}
 
   async ngAfterViewInit() {
     await this.initAndStartWrapper();
     runInInjectionContext(this.injector, this.setupEditorBehaviours.bind(this));
     await this.startLanguageClient();
     this.editorIsReady.emit(this.wrapper.getEditor());
-  }
-
-  ngOnDestroy() {
-    this.componentDestroyed.next();
   }
 
   private async startLanguageClient() {
@@ -63,7 +58,7 @@ export class MonacoEditorComponent implements AfterViewInit, OnDestroy {
     const editor = this.wrapper.getEditor();
     if(editor === undefined) throw Error('Monaco Editor Wrapper returned no editor!');
     editor.updateOptions(this.editorOptions());
-    new MonacoEditorBehaviours(this.componentDestroyed, editor, this.editorSizeChange(), this.editorContent,
+    new MonacoEditorBehaviours(this.destroyRef, editor, this.editorSizeChange(), this.editorContent,
       this.onEditorContentChanged.bind(this));
   }
 
@@ -75,6 +70,6 @@ export class MonacoEditorComponent implements AfterViewInit, OnDestroy {
     const wrapperConfig = MonacoWrapperConfig.buildWrapperUserConfig(this.editorElement.nativeElement, this.editorContent(),
       this.unitId());
     await this.wrapper.initAndStart(wrapperConfig, false);
-    this.componentDestroyed.pipe(take(1)).subscribe(() => this.wrapper.dispose());
+    this.destroyRef.onDestroy(() => this.wrapper.dispose());
   }
 }
