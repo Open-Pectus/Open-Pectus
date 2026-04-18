bc8bd3f5a1173276c0de793f6187ed8bd6f8f298
Eskild Schroll-Fleischer <eskildsf@users.noreply.github.com>
2026-04-18 21:43:10 +0200

    Avoid merging system and UOD tags on every write of process image.

--- openpectus/engine/engine.py
+++ openpectus/engine/engine.py
@@ -445,10 +445,8 @@ class Engine(InterpreterContext):
         hwl = self.uod.hwl
         register_values = []
         registers = [r for r in hwl.registers.values() if RegisterDirection.Write in r.direction]
-        assert self.uod.system_tags
-        tags = self.uod.tags.merge_with(self.uod.system_tags)
         for r in registers:
-            tag_value = tags[r.name].get_value()
+            tag_value = self._tags[r.name].get_value()
             register_value = tag_value if "from_tag" not in r.options \
                 else r.options["from_tag"](tag_value)
             register_values.append(register_value)
